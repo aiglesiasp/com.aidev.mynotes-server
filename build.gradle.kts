f52121f0647b932217e28fefcfbd1d0845909608
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-07 23:42:01 +0200

    Agregadas dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,22 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
+    application
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
     id("io.ktor.plugin") version "2.3.9"
+    id("com.squareup.sqldelight") version "1.5.4"
 }
 
 group = "com.aidev"
 version = "0.0.1"
 
+
 application {
     mainClass.set("com.aidev.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
+    //val isDevelopment: Boolean = true
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
@@ -23,9 +28,18 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-html-builder:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-cors:$ktor_version")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+
+    implementation("com.squareup.sqldelight:sqlite-driver:1.5.4")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
 }
