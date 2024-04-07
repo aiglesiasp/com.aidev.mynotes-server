1a86bfb374930a89f8bbd232c648cc324ea0c6d3
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-07 23:51:20 +0200

    Aplicando Funciones de Alto Nivel para refactorizar NotesRepository

--- src/main/kotlin/com/aidev/repositories/NotesRepository.kt
+++ src/main/kotlin/com/aidev/repositories/NotesRepository.kt
@@ -1,7 +1,6 @@
 package com.aidev.repositories
 
 import com.aidev.model.Note
-import com.aidev.model.Note.NoteType.*
 
 object NotesRepository {
 
@@ -9,47 +8,27 @@ object NotesRepository {
     private var currentId = 1L
 
     // OBTENER LISTA COMPLETA
-    fun getAll(): List<Note> {
-        //Devolvemos la lista completa
-        return list
-    }
+    fun getAll(): List<Note> = list
 
     // OBTENER NOTA POR ID
-    fun getNoteById(id: Long): Note? {
-        //Buscamos en la lsita por ese id
-        val note: Note? = list.find { it.id == id }
-        return note
-    }
+    fun getNoteById(id: Long): Note? = list.find { it.id == id }
 
     // CREAR NOTA
-    fun save(note: Note): Note {
-        // Creamos nota con el currentId
-        val newNote = note.copy(id = currentId)
-        currentId++
-        //Agregamos nota a la lista
-        list.add(newNote)
-        return newNote
-    }
+    fun save(note: Note): Note =
+       note.copy(id = currentId++)
+           .also(list::add)
 
     // ACTUALIZAR NOTA
-    fun update(note: Note): Boolean {
-        //Obtener indice de la nota
-        val index = list.indexOfFirst { it.id == note.id }
-        //Si no lo encontramos devolvemos false
-        if(index<0) return false
-        //Sino actualizamos la nota
-        list[index] = note
-        return true
-    }
+    fun update(note: Note): Boolean =
+        list.indexOfFirst { it.id == note.id }
+            .takeIf { it > 0 }
+            ?.also {  list[it] = note }
+            .let { it != null }
 
     // ELIMINAR NOTA
-    fun delete(id: Long): Boolean {
-        //Obtener indice de la nota
-        val index = list.indexOfFirst { it.id == id }
-        //Si no lo encontramos devolvemos false
-        if(index<0) return false
-        //Sino eliminamos la nota
-        list.removeAt(index)
-        return true
-    }
+    fun delete(id: Long): Boolean =
+        list.indexOfFirst { it.id == id }
+            .takeIf { it > 0 }
+            ?.also(list::removeAt)
+            .let { it != null }
 }
