b32bf0caa17b35b910b94685028403f699ecf4a6
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-07 23:43:23 +0200

    Modificado Routing para consumir desde Servidor

--- src/main/kotlin/com/aidev/plugins/Routing.kt
+++ src/main/kotlin/com/aidev/plugins/Routing.kt
@@ -1,13 +1,28 @@
 package com.aidev.plugins
 
+import com.aidev.repositories.NotesRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
+    // Starting point for a Ktor app
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        route("/notes") {
+
+            // CREATE
+
+            //READ
+            get {
+                call.respond(NotesRepository.getAll())
+            }
+
+            //UPDATE
+
+            //DELETE
         }
     }
 }
